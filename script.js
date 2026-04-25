1d1fa15fcb54f3e2504c192ca0d7a1e1f2348b15
CookiFlo <flordario1994@gmail.com>
2026-04-25 13:56:15 +0200

    Update script.js

--- script.js
+++ script.js
@@ -12,3 +12,33 @@ document.body.style.overflow = "auto"
 window.scrollTo(0,0)
 
 }
+
+// BAJAR A SECCION 2
+function bajar(){
+document.getElementById("hoja2").scrollIntoView({behavior:"smooth"})
+}
+
+
+// CONTADOR
+
+const fechaBoda = new Date("2027-01-15T00:00:00")
+
+function actualizarContador(){
+
+const ahora = new Date()
+
+const diferencia = fechaBoda - ahora
+
+const dias = Math.floor(diferencia / (1000*60*60*24))
+const horas = Math.floor((diferencia / (1000*60*60)) % 24)
+const minutos = Math.floor((diferencia / (1000*60)) % 60)
+const segundos = Math.floor((diferencia / 1000) % 60)
+
+document.getElementById("dias").innerHTML = dias
+document.getElementById("horas").innerHTML = horas
+document.getElementById("minutos").innerHTML = minutos
+document.getElementById("segundos").innerHTML = segundos
+
+}
+
+setInterval(actualizarContador,1000)
