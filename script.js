c9c0a085eec52dea6c51a15c12715a671b3237a7
CookiFlo <flordario1994@gmail.com>
2026-04-25 13:39:11 +0200

    Update script.js

--- script.js
+++ script.js
@@ -4,6 +4,11 @@ document.getElementById("intro").style.display="none"
 
 document.getElementById("hoja1").classList.remove("hidden")
 
+document.getElementById("hoja2").classList.remove("hidden")
+
+// activar scroll
+document.body.style.overflow = "auto"
+
 window.scrollTo(0,0)
 
 }
