f5f74a9f0b9e4a8a2ab586b2ca20f6d3724ae584
CookiFlo <flordario1994@gmail.com>
2026-04-26 18:14:10 +0200

    Update script.js

--- script.js
+++ script.js
@@ -3,8 +3,9 @@ function abrirSobre(){
 const intro = document.getElementById("intro")
 const hoja1 = document.getElementById("hoja1")
 const hoja2 = document.getElementById("hoja2")
+const hoja3 = document.getElementById("hoja3")
+const hoja4 = document.getElementById("hoja4")
 
-// animación (zoom + fade)
 intro.classList.add("ocultar")
 
 setTimeout(()=>{
@@ -13,24 +14,31 @@ intro.style.display="none"
 
 hoja1.classList.remove("hidden")
 hoja2.classList.remove("hidden")
+hoja3.classList.remove("hidden")
+hoja4.classList.remove("hidden")
 
-// entrada elegante
 setTimeout(()=>{
 hoja1.classList.add("mostrar")
 },50)
 
-// activar scroll
 document.body.style.overflow = "auto"
-
 window.scrollTo(0,0)
 
 },300)
 
 }
 
-// BAJAR A SECCION 2
+// scroll
 function bajar(){
 document.getElementById("hoja2").scrollIntoView({behavior:"smooth"})
+}
+
+function bajarA3(){
+document.getElementById("hoja3").scrollIntoView({behavior:"smooth"})
+}
+
+function bajarA4(){
+document.getElementById("hoja4").scrollIntoView({behavior:"smooth"})
 }
 
 
@@ -41,7 +49,6 @@ const fechaBoda = new Date("2027-01-15T00:00:00")
 function actualizarContador(){
 
 const ahora = new Date()
-
 const diferencia = fechaBoda - ahora
 
 const dias = Math.floor(diferencia / (1000*60*60*24))
@@ -57,3 +64,20 @@ document.getElementById("segundos").innerHTML = segundos
 }
 
 setInterval(actualizarContador,1000)
+
+
+// SWIPE izquierda hoja3 → hoja4
+
+let startX = 0
+
+document.getElementById("hoja3").addEventListener("touchstart", e=>{
+startX = e.touches[0].clientX
+})
+
+document.getElementById("hoja3").addEventListener("touchend", e=>{
+let endX = e.changedTouches[0].clientX
+
+if(startX - endX > 50){
+document.getElementById("hoja4").scrollIntoView({behavior:"smooth"})
+}
+})
