1456515d7e706d501d809e595177bc3b4a73e0ba
CookiFlo <flordario1994@gmail.com>
2026-05-16 19:40:23 +0200

    Update script.js

--- script.js
+++ script.js
@@ -17,18 +17,14 @@ hoja2.classList.remove("hidden")
 hoja3.classList.remove("hidden")
 hoja4.classList.remove("hidden")
 
-setTimeout(()=>{
 hoja1.classList.add("mostrar")
-},50)
 
-document.body.style.overflow = "auto"
-window.scrollTo(0,0)
+document.body.style.overflow="auto"
 
 },300)
 
 }
 
-// scroll
 function bajar(){
 document.getElementById("hoja2").scrollIntoView({behavior:"smooth"})
 }
@@ -38,11 +34,8 @@ document.getElementById("hoja3").scrollIntoView({behavior:"smooth"})
 }
 
 function bajarA4(){
-document.getElementById("hoja4").scrollIntoView({behavior:"smooth"})
+document.getElementById("hoja4").classList.add("mostrar4")
 }
-
-
-// CONTADOR
 
 const fechaBoda = new Date("2027-01-15T00:00:00")
 
@@ -51,33 +44,19 @@ function actualizarContador(){
 const ahora = new Date()
 const diferencia = fechaBoda - ahora
 
-const dias = Math.floor(diferencia / (1000*60*60*24))
-const horas = Math.floor((diferencia / (1000*60*60)) % 24)
-const minutos = Math.floor((diferencia / (1000*60)) % 60)
-const segundos = Math.floor((diferencia / 1000) % 60)
+document.getElementById("dias").innerHTML =
+Math.floor(diferencia / (1000*60*60*24))
 
-document.getElementById("dias").innerHTML = dias
-document.getElementById("horas").innerHTML = horas
-document.getElementById("minutos").innerHTML = minutos
-document.getElementById("segundos").innerHTML = segundos
+document.getElementById("horas").innerHTML =
+Math.floor((diferencia / (1000*60*60)) % 24)
+
+document.getElementById("minutos").innerHTML =
+Math.floor((diferencia / (1000*60)) % 60)
+
+document.getElementById("segundos").innerHTML =
+Math.floor((diferencia / 1000) % 60)
 
 }
 
+actualizarContador()
 setInterval(actualizarContador,1000)
-
-
-// SWIPE izquierda hoja3 → hoja4
-
-let startX = 0
-
-document.getElementById("hoja3").addEventListener("touchstart", e=>{
-startX = e.touches[0].clientX
-})
-
-document.getElementById("hoja3").addEventListener("touchend", e=>{
-let endX = e.changedTouches[0].clientX
-
-if(startX - endX > 50){
-document.getElementById("hoja4").scrollIntoView({behavior:"smooth"})
-}
-})
