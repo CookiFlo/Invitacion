d1a87972d271634404cdb059a206fa1fc10f25fc
CookiFlo <flordario1994@gmail.com>
2026-05-16 20:02:51 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,25 +1,21 @@
 function abrirSobre(){
 
-const intro = document.getElementById("intro")
-const hoja1 = document.getElementById("hoja1")
-const hoja2 = document.getElementById("hoja2")
-const hoja3 = document.getElementById("hoja3")
-const hoja4 = document.getElementById("hoja4")
+const intro=document.getElementById("intro")
+const hojas=document.querySelectorAll("section")
 
 intro.classList.add("ocultar")
 
 setTimeout(()=>{
-
 intro.style.display="none"
 
-hoja1.classList.remove("hidden")
-hoja2.classList.remove("hidden")
-hoja3.classList.remove("hidden")
-hoja4.classList.remove("hidden")
-
-hoja1.classList.add("mostrar")
+hojas.forEach(h=>{
+if(h.id!=="intro"){
+h.classList.remove("hidden")
+}
+})
 
 document.body.style.overflow="auto"
+window.scrollTo(0,0)
 
 },300)
 }
@@ -32,28 +28,41 @@ function bajarA3(){
 document.getElementById("hoja3").scrollIntoView({behavior:"smooth"})
 }
 
-function bajarA4(){
-document.getElementById("hoja4").classList.add("mostrar4")
+function irHoja4(){
+document.getElementById("hoja4").scrollIntoView({behavior:"smooth"})
 }
 
-const fechaBoda = new Date("2027-01-15T00:00:00")
+function volverHoja3(){
+document.getElementById("hoja3").scrollIntoView({behavior:"smooth"})
+}
+
+function irHoja5(){
+document.getElementById("hoja5").scrollIntoView({behavior:"smooth"})
+}
+
+function volverHoja4(){
+document.getElementById("hoja4").scrollIntoView({behavior:"smooth"})
+}
+
+function irHoja6(){
+document.getElementById("hoja6").scrollIntoView({behavior:"smooth"})
+}
+
+function volverHoja5(){
+document.getElementById("hoja5").scrollIntoView({behavior:"smooth"})
+}
+
+const fechaBoda=new Date("2027-01-15T00:00:00")
 
 function actualizarContador(){
 
-const ahora = new Date()
-const diferencia = fechaBoda - ahora
+const ahora=new Date()
+const diferencia=fechaBoda-ahora
 
-document.getElementById("dias").innerHTML =
-Math.floor(diferencia/(1000*60*60*24))
-
-document.getElementById("horas").innerHTML =
-Math.floor((diferencia/(1000*60*60))%24)
-
-document.getElementById("minutos").innerHTML =
-Math.floor((diferencia/(1000*60))%60)
-
-document.getElementById("segundos").innerHTML =
-Math.floor((diferencia/1000)%60)
+document.getElementById("dias").innerHTML=Math.floor(diferencia/(1000*60*60*24))
+document.getElementById("horas").innerHTML=Math.floor((diferencia/(1000*60*60))%24)
+document.getElementById("minutos").innerHTML=Math.floor((diferencia/(1000*60))%60)
+document.getElementById("segundos").innerHTML=Math.floor((diferencia/1000)%60)
 }
 
 actualizarContador()
